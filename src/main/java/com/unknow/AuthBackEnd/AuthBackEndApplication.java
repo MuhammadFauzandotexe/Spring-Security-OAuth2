d1dd9661062ff5bd6a19fc707fc4defe42b88387
MuhammadFauzandotexe <mfauzan51087@gmail.com>
2023-08-24 00:54:34 +0700

    Initial commit

---
+++ src/main/java/com/unknow/AuthBackEnd/AuthBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.unknow.AuthBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unknow/AuthBackEnd/AuthBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unknow.AuthBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
