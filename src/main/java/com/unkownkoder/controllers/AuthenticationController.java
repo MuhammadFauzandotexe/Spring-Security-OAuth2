c2d85778892bfe0ab10c618c4466a5eb3ce48c76
MuhammadFauzandotexe <mfauzan51087@gmail.com>
2024-01-23 17:46:31 +0700

    first commit

--- src/main/java/com/unkownkoder/controllers/AuthenticationController.java
+++ src/main/java/com/unkownkoder/controllers/AuthenticationController.java
@@ -1,10 +1,8 @@
 package com.unkownkoder.controllers;
 
-import com.unkownkoder.models.User;
 import com.unkownkoder.repository.UserLdapRepository;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,13 +11,10 @@ import com.unkownkoder.models.LoginResponseDTO;
 import com.unkownkoder.models.RegistrationDTO;
 import com.unkownkoder.services.AuthenticationService;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/auth")
 @CrossOrigin("*")
 @AllArgsConstructor
-@Slf4j
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
@@ -28,16 +23,14 @@ public class AuthenticationController {
     public ApplicationUser registerUser(@RequestBody RegistrationDTO body){
         return authenticationService.registerUser(body.getUsername(), body.getPassword());
     }
-    
+
     @PostMapping("/login")
     public LoginResponseDTO loginUser(@RequestBody RegistrationDTO body){
         return authenticationService.loginUser(body.getUsername(), body.getPassword());
     }
 
-    @GetMapping("/ldap-user")
+    @GetMapping("/get-ldap-user")
     private String getLdap(){
-        List<User> ram = userLdapRepository.findByUsernameLikeIgnoreCase("ram");
-        log.info(String.valueOf(ram.size()));
-        return "done";
+        userLdapRepository.l
     }
-}   
+}
